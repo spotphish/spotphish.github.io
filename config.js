d5359125a2b3a36e7757a3399b5f9d3bbbc8f8a3
vijays1 <vijay.sharma@coriolis.co.in>
2020-11-26 11:22:31 +0530

    Fake facebook images added

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var algorithm_tensorflow_graph_model_url = "https://raw.githubusercontent.com/sudhirkr/spotphish-model/master/web_model/model.json";
 var algorithm_tensorflow_labels = ["Axis Bank", "Bank of Baroda","HDFC Bank","State Bank of India","Kotak Bank","ICICI Bank","Bandhan Bank","Federal Bank", "IndusInd Bank", "Punjab National Bank","RBL Bank","Facebook","Amazon","Paypal","Google","Dropbox","Bank of India","Bank of Maharashtra","Canara Bank","Central Bank of India","Indian Bank","Indian Overseas Bank","Punjab and Sind Bank","Union Bank of India","UCO Bank","Yes Bank","IDBI Bank","LinkedIn","Twitter","incometax"];
 var system_log_server_url = "http://34.87.101.217:4000/spotphishes.json";
-var test_dataset_url = "https://raw.githubusercontent.com/sudhirkr/spotphish-model/master/load-dataset.json";
+var test_dataset_url = "https://raw.githubusercontent.com/sudhirkr/spotphish-model/vijay/load-dataset.json";
